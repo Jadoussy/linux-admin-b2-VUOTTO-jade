abf71b6008432f69f1bf9589289de56be77e944f
Jadoussy <vuotto.jade@gmail.com>
2026-06-18 16:33:32 +0200

    feat(m5): scrip tharden_ssh : durcicement du protocle SSH et pare feu UFW

--- scripts/harden_ssh.sh
+++ scripts/harden_ssh.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 # ============================================================
 # Script  : harden_ssh.sh
-# Auteur  : Prénom NOM
-# Date    : YYYY-MM-DD
+# Auteur  : Vuotto Jade
+# Date    : 2026-06-18
 # Desc    : Durcissement de la configuration SSH
-# Usage   : sudo ./scripts/harden_ssh.sh
+# Usage   :     chmod +x harden_ssh.sh
+#               sudo ./scripts/harden_ssh.sh
 # ⚠  IMPORTANT : Ayez une deuxième session SSH ouverte
 #                avant d'exécuter ce script !
 # ============================================================
 
 # === Vérification root =======================================
-# TODO : vérifier root, exit 1 sinon
-
-
+# Si le script n'est pas lancé (-ne) en root : 
+if [ "$EUID" -ne 0 ]; then
+    # Alors, on affiche un message d'erreur et quitte 
+    echo "Le script doit être exécuté avec sudo (privilèges)" >&2
+    exit 1
+fi
 
 # === Variables ===============================================
 SSHD_CONFIG="/etc/ssh/sshd_config"
@@ -21,7 +25,9 @@ BACKUP="$SSHD_CONFIG.bak.$(date +%Y%m%d)"
 
 # === Sauvegarde ==============================================
 echo "[INFO] Sauvegarde de $SSHD_CONFIG → $BACKUP"
-# TODO : copier sshd_config vers $BACKUP
+
+# copier sshd_config vers $BACKUP
+cp "$SSHD_CONFIG" "$BACKUP"
 
 
 
@@ -29,7 +35,10 @@ echo "[INFO] Sauvegarde de $SSHD_CONFIG → $BACKUP"
 restore_and_exit() {
     echo "[ERREUR] La configuration SSH est invalide." >&2
     echo "[INFO] Restauration de la sauvegarde..." >&2
-    # TODO : restaurer $BACKUP vers $SSHD_CONFIG
+
+    # Restaure $BACKUP vers $SSHD_CONFIG
+    cp "$BACKUP" "$SSHD_CONFIG"
+
     echo "[INFO] Sauvegarde restaurée. Aucune modification appliquée." >&2
     exit 1
 }
@@ -40,38 +49,45 @@ restore_and_exit() {
 set_directive() {
     local key="$1"
     local value="$2"
-    # TODO : si la directive (commentée ou non) existe, la remplacer
-    #        sinon, l'ajouter à la fin du fichier
-    #        Utiliser sed -i pour la modification en place
+
+    # Vérifie si la clé n'existe pas déjà
+    if grep -qE "^#? *${key} " "$SSHD_CONFIG"; then
+        # Remplace la ligne existante par la nouvelle clé/valeur propre
+        sed -i -E "s/^#? *${key} .*/${key} ${value}/" "$SSHD_CONFIG"
+    else
+        # Sinon (= la clé n'existe pas du tout), elle est rajoutée à la fin du fichier
+        echo "${key} ${value}" >> "$SSHD_CONFIG"
+    fi
 }
 
 echo "[INFO] Application des directives de sécurité..."
 
-# TODO : appeler set_directive pour chaque paramètre :
-# PermitRootLogin       no
-# PasswordAuthentication no
-# PubkeyAuthentication  yes
-# MaxAuthTries          3
-# LoginGraceTime        20
-# ClientAliveInterval   300
-# ClientAliveCountMax   2
-# X11Forwarding         no
-
-
+# Appeler set_directive pour chaque paramètre :
+set_directive "PermitRootLogin" "no"
+set_directive "PasswordAuthentication" "no"
+set_directive "PubkeyAuthentication" "yes"
+set_directive "MaxAuthTries" "3"
+set_directive "LoginGraceTime" "20"
+set_directive "ClientAliveInterval" "300"
+set_directive "ClientAliveCountMax" "2"
+set_directive "X11Forwarding" "no"
 
 # === Validation ==============================================
 echo "[INFO] Validation de la syntaxe..."
-# TODO : lancer sshd -t
-#        Si le code retour est non nul, appeler restore_and_exit
 
-
+# Si le code retour est non nul, appeler restore_and_exit
+if ! sshd -t; then
+    restore_and_exit
+fi     
 
 # === Résumé des modifications ================================
 echo ""
 echo "============================================================"
 echo " Modifications appliquées (diff)"
 echo "============================================================"
-# TODO : afficher le diff entre $BACKUP et $SSHD_CONFIG
+# Affiche le diff entre $BACKUP et $SSHD_CONFIG
+diff "$BACKUP" "$SSHD_CONFIG" || true
+# true évite que le script s'arrète brutalement si le diff détecte des changements
 
 
 
